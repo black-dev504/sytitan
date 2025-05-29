de2e49b2606006e58326b8fc7af0cfa72c4290fd
blackdev <blackdev504@gmail.com>
2025-05-29 16:42:07 +0100

    upd

--- src/auth.js
+++ src/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://sytitan.vercel.app',
+  baseURL: 'https://sytitan.onrender.com',
   withCredentials: true // 👈 include cookies with requests
 });
 
