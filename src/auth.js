01ca6af3cad245a77ebfb88b92563a112cb4dab7
usman sy <blackdev504@gmail.com>
2025-06-17 17:02:06 +0100

    update

--- src/auth.js
+++ src/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // your production backend
+  baseURL: "https://sytitan.onrender.com", // your production backend
   withCredentials: true,
 });
 export const contact = async (credentials) => {
