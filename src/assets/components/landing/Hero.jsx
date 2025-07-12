0a35e2cf878abc5910b421198ec470f92dd6907d
USMAN_SY <USSY@USSYS-MACBOOK-PRO.local>
2025-07-12 22:37:26 +0100

    updt

--- src/assets/components/landing/Hero.jsx
+++ src/assets/components/landing/Hero.jsx
@@ -8,31 +8,31 @@ const navigate = useNavigate();
     <section className=' bg-black text-white'>
       <div className='grid grid-rows-1 items-center lg:grid-cols-2'>
         {/* Left Section */}
-        <div className='flex flex-col justify-start mt-25 mx-5 px-10 h-fit py-14 bg-[#252525] opacity-90 rounded-3xl lg:ml-20 '>
-          <h1 className='text-5xl font-semibold'>
+        <div className='flex flex-col justify-start mt-15 md:mt-25 mx-5 px-5 md:px-10 h-fit py-14 bg-[#252525] opacity-90 rounded-3xl lg:ml-20 '>
+          <h1 className='md:text-5xl text-3xl font-semibold'>
             Purebred Passion Starts Here — Welcome to <span className='text-primary'>SY TITAN.</span>
           </h1>
-          <p className='my-6 text-xl font-normal'>
+          <p className='my-6 text-[18px] md:text-xl font-normal'>
             Proudly raising healthy, well-socialized American Bully dogs with a focus on lineage, temperament, and lifelong companionship.
           </p>
 
-          <div className='flex gap-12'>
+          <div className='flex gap-5'>
             <div className="flex flex-col justify-start">
-              <h1 className='text-3xl font-semibold text-white'>6+</h1>
+              <h1 className='text-2xl md:text-3xl font-semibold text-white'>6+</h1>
               <div className='my-2 w-16 h-[3px] bg-white'></div>
-              <p>Years of Breeding Experience</p>
+              <p className='text-xs md:text-xl'>Years of Breeding Experience</p>
             </div>
 
             <div className="flex flex-col justify-start">
-              <h1 className='text-3xl font-semibold text-white'>50+</h1>
+              <h1 className='text-2xl md:text-3xl  font-semibold text-white'>50+</h1>
               <div className='my-2 w-16 h-[3px] bg-white'></div>
-              <p>Puppies Raised in Loving Homes</p>
+              <p className='text-xs md:text-xl'>Puppies Raised in Loving Homes</p>
             </div>
 
             <div className="flex flex-col justify-start">
-              <h1 className='text-3xl font-semibold text-white'>100%</h1>
+              <h1 className='text-2xl md:text-3xl  font-semibold text-white'>100%</h1>
               <div className='my-2 w-16 h-[3px] bg-white'></div>
-              <p>Ethical & Health-Screened Lineage</p>
+              <p className='text-xs md:text-xl'>Ethical & Health-Screened Lineage</p>
             </div>
           </div>
 
--- src/assets/components/landing/Journey.jsx
+++ src/assets/components/landing/Journey.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Journey = () => {
   return (
    <section className='bg-secondary'>
-  <div className="grid grid-cols-1 lg:grid-cols-2  py-15 gap-20 ml-5 lg:ml-25 items-center">
+  <div className="grid grid-cols-1 lg:grid-cols-2  py-15 gap-20 md:ml-5 lg:ml-25 items-center">
     
     {/* Text Content */}
     <div className="flex items-center lg:items-start px-5 text-center lg:text-start flex-col">
       <h1 className='text-5xl text-[#ECECEC] font-semibold py-12'>Our Journey</h1>
-      <p className='text-2xl font-normal text-white'>
+      <p className='text-xl md:text-2xl font-normal text-white'>
         SY TITAN is a dedicated breeding kennel based in Paradis Island, focused on producing exceptional American Bullies with strong lineage, stable temperaments, and striking appearance. Located in the quiet countryside of Japan, our dogs are raised in a clean, well-structured environment with early socialization and plenty of space to thrive. We believe in ethical breeding practices and connecting responsible owners with companions raised with care, purpose, and pride.
       </p>
       <button className='px-9 py-6 hover:border-1 max-w-[200px] mt-6 cursor-pointer rounded-[40px] bg-primary text-white'>
